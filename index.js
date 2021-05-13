765c7d930285e7852fc84965e7f1190b61113e6d
Justin Myers <jmyers.sde@gmail.com>
2021-05-13 16:25:26 -0400

    Portfolio

---
+++ index.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip();
+  });
+
+
+// hideShow = () => {
+//     var x = document.getElementById("myDIV");
+//     if (x.style.display === "none") {
+//       x.style.display = "block";
+//     } else {
+//       x.style.display = "none";
+//     }
+//   }

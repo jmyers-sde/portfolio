58c783ace1254bcaa303132b605a901c2f5f0717
Justin Myers <justin@MacBook-Air.local>
2022-01-09 19:04:21 -0500

    last test

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
 $(document).ready(function(){
     $('[data-toggle="tooltip"]').tooltip();
   });
-
-
-// hideShow = () => {
-//     var x = document.getElementById("myDIV");
-//     if (x.style.display === "none") {
-//       x.style.display = "block";
-//     } else {
-//       x.style.display = "none";
-//     }
-//   }
